fae38deb7bf2b3146c347f3e73209118b3d23971
DaniAkash <s.daniakash@gmail.com>
2017-03-05 11:13:57 +0530

    Task services

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { TASKS } from "../../data/tasks";
+import { TASKS } from "../../data/TASKS";
 
 @Component({
   selector: 'app-task-list',
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Task } from "../dataTypes/Task";
-import { TASKS } from "../data/tasks";
+import { TASKS } from "../data/TASKS";
 
 @Injectable()
 export class TaskService {
